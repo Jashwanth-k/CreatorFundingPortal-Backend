110c3444ed060aa8b6f4499a63dd3fb7518e4b44
jashwanth <jashwanth328910@gmail.com>
2023-01-24 17:57:51 +0530

    updating res headers in server.js

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const authRouter = require("./routes/auth.route");
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-api-key"
+  );
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
