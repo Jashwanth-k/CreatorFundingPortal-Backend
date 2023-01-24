c909c7d3565af9b8b19d669abff60cc6bde874f9
jashwanth <jashwanth328910@gmail.com>
2023-01-24 18:25:50 +0530

    modifying cors in app.js

--- app.js
+++ app.js
@@ -3,5 +3,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 app.options("*", cors());
+
+app.use((req, res, next) => {
+  res.setHeader("access-control-allow-credentials", true);
+  res.setHeader(
+    "access-control-allow-headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization,x-api-key"
+  );
+  res.setHeader("access-control-allow-methods", "GET, POST, PUT, DELETE");
+  res.setHeader("access-control-allow-origin", "*");
+  next();
+});
+
 app.use(bodyParser.json());
 module.exports = { app, express };
