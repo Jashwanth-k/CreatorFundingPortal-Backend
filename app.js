c2066c1a6c62574dd0cf102bc2d25438fd4d0a04
jashwanth <jashwanth328910@gmail.com>
2023-01-24 17:37:31 +0530

    adding cors in app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(cors());
 app.use(bodyParser.json());
 module.exports = { app, express };
