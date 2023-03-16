0d02a1300c80905bd2e92de7cba25f334020827a
jashwanth <jashwanth328910@gmail.com>
2023-03-16 21:08:39 +0530

    added author in headers

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, authorization, Authorization"
   );
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", true);
