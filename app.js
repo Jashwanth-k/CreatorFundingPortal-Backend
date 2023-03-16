4ddfdc121117ab8ef9df19325e2ca0989f4d8950
jashwanth <jashwanth328910@gmail.com>
2023-03-16 23:33:29 +0530

    modified app.js

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, authorization, Authorization"
-  );
+  res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", true);
   next();
