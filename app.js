d1822710d23b5481fc5b48ec95dcc634c81994fc
jashwanth-k <jashwanth328910@gmail.com>
2023-04-22 19:19:52 +0530

    reverted no-cache headers

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const nocache = require("nocache");
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(nocache());
-app.set("etag", false);
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
